27324afd5d559c61dcea1572262a7b3eec3e6bdc
Rich Staats <rich@secretstache.com>
2017-05-12 16:04:45 -0600

    Adds Archive widget back to widget_init

--- modules/admin-setup.php
+++ modules/admin-setup.php
@@ -51,7 +51,7 @@ function remove_widgets() {
 
   unregister_widget( 'WP_Widget_Pages' );
   unregister_widget( 'WP_Widget_Calendar' );
-  unregister_widget( 'WP_Widget_Archives' );
+  // unregister_widget( 'WP_Widget_Archives' );
   unregister_widget( 'WP_Widget_Meta' );
   unregister_widget( 'WP_Widget_Recent_Posts' );
   unregister_widget( 'WP_Widget_Recent_Comments' );
